6ee7a0526083c31e4e7a4508485ff3a49791cd6c
Jagdeep Singh <jagdeeeeep@gmail.com>
2019-05-15 23:41:16 -0700

    challenges complete, tests passing

--- code-challenges/401/array/arrayBinarySearch/__test__/array-binary-search.test.js
+++ code-challenges/401/array/arrayBinarySearch/__test__/array-binary-search.test.js
@@ -4,20 +4,38 @@
 const arrayBinarySearch = require('../array-binary-search.js');
 
 describe('array binary search expected output', () => {
-  xit('searching even length array', () => {
+  it('searching even length array', () => {
     let arr = [0, 1, 2, 3, 4, 5];
     expect(arrayBinarySearch(arr, 2)).toEqual(2);
     expect(arrayBinarySearch(arr, 10)).toEqual(-1);
   });
-  xit('searching odd length array', () => {
+  it('searching odd length array', () => {
     let arr = [0, 1, 2, 3, 4, 5, 6];
     expect(arrayBinarySearch(arr, 2)).toEqual(2);
     expect(arrayBinarySearch(arr, 10)).toEqual(-1);
+  });
+
+  it('given example 1', () => {
+    let arr = [4, 8, 15, 16, 23, 42];
+    expect(arrayBinarySearch(arr, 15)).toEqual(2);
+  });
+
+  it('given example 2', () => {
+    let arr = [11,22,33,44,55,66,77];
+    expect(arrayBinarySearch(arr, 90)).toEqual(-1);
   });
 });
 
 describe('array binary search expected failure', () => {
   it('missing parameters', () => {
-    expect(arrayBinarySearch()).toThrowError(TypeError);
+    
+    expect(() => { arrayBinarySearch(); }).toThrow(TypeError);
+    expect(() => { arrayBinarySearch([]); }).toThrow(TypeError);
+    expect(arrayBinarySearch([], 0)).toEqual(-1);
+    expect(arrayBinarySearch([1], 1)).toEqual(0);
+    expect(arrayBinarySearch([1], 3)).toEqual(-1);
+
+
+
   });
 });
--- code-challenges/401/array/arrayBinarySearch/array-binary-search.js
+++ code-challenges/401/array/arrayBinarySearch/array-binary-search.js
@@ -6,27 +6,39 @@
  *  @param {*} value, search key
  *  @returns {Number} -1 or index of search key
  */
-module.exports = function(arr, value) {
+module.exports = exports = (arr, value) => {
   if (!arr || (!value && value !== 0)) {
-    throw TypeError();
+    throw new TypeError('Invalid Parameters');
   } else if (arr.length === 0) {
     return -1;
   } else if (arr.length === 1) {
     return arr[0] === value ? 0 : -1;
-  } else {
   }
+  return search(arr, value, 0, arr.length - 1);
 };
 
 /**
- *
+ * searches an array for a given value between given indexes
+ * 
  * @param {Array} arr, sorted array
  * @param {*} val, search key
  * @param {Number} left, left index
  * @param {Number} right, right index
  * @returns {Number} -1 or index
  */
-function search(arr, val, start, end) {
-  if (end - start > 1) {
+function search(arr, val, left, right) {
+  if (right - left < 0) {
+    return -1;
+  }
+  let middle = Math.floor((left + right) / 2);
+
+  if (arr[middle] === val) {
+    return middle;
+  } else if (arr[middle] > val) {
+    // check left side of middle
+    return search(arr, val, left, middle - 1);
   } else {
+    // check right side of middle
+    return search(arr, val, middle + 1, right);
   }
 }
