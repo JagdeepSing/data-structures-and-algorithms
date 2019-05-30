84e8c14d91cb42498592a8083814836a9a32e423
Jagdeep Singh <jagdeeeeep@gmail.com>
2019-05-29 17:41:39 -0700

    Challenge complete, readme needs to be completed

---
+++ code-challenges/401/stacks-and-queues/fifoAnimalShelter/README.md
@@ -0,0 +1,18 @@
+# Challenge Summary
+
+An animal shelter for dogs and cats wants to make sure no dog or cat spends too much time at the shelter. To do this they only let you adopt the dog or cat that has been at the shelter the longest.
+
+## Challenge Description
+
+Create a class called AnimalShelter which holds only dogs and cats. The shelter operates using a first-in, first-out approach.
+
+Implement the following methods:
+- `enqueue(animal)`: adds animal to the shelter. animal can be either a dog or a cat object.
+- `dequeue(pref)`: returns either a dog or a cat. If pref is not "dog" or "cat" then return null.
+
+## Approach & Efficiency
+
+
+
+## Solution
+<!-- Embedded whiteboard image -->
---
+++ code-challenges/401/stacks-and-queues/fifoAnimalShelter/fifo-animal-shelter.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const Queue = require('../../../../data-structures/stacksAndQueues/lib/stacks-and-queues.js').Queue;
+
+class AnimalShelter {
+  constructor() {
+    this.dogs = new Queue();
+    this.cats = new Queue();
+    this.count = 0;
+  }
+
+  /**
+   * Adds animal to shelter if of type 'dog' or 'cat'
+   * If of some other type, return an exception.
+   *
+   * @param {Object} animal animal object with property type
+   * @returns {Number} number of animals in shelter
+   * @throws {TypeError} shelter only accepts dogs or cats
+   */
+  enqueue(animal) {
+    if (!animal.type || (animal.type !== 'dog' && animal.type !== 'cat')) {
+      throw new TypeError(`wrong shelter, we don't accept ${animal.type}s`);
+    }
+    if (animal.type === 'dog') {
+      animal.num = ++this.count;
+      this.dogs.enqueue(animal);
+    } else if (animal.type === 'cat') {
+      animal.num = ++this.count;
+      this.cats.enqueue(animal);
+    }
+    return this.dogs.length + this.cats.length;
+  }
+
+  /**
+   * Removes most senior animal of passed in preference
+   * If preference is empty string, returns most senior animal out of both cats and dogs
+   * If type is something other than 'dog', 'cat' or '', returns null
+   *
+   * @param {string} pref preference for animal type
+   * @returns {Object|null} animal object if animal of preference type is available, null otherwise
+   */
+  dequeue(pref) {
+    if (pref === '') {
+      if (!this.dogs.peek()) {
+        return this.cats.dequeue();
+      }
+      if (!this.cats.peek()) {
+        return this.dogs.dequeue();
+      }
+      if (this.dogs.peek().num <= this.cats.peek().num) {
+        return this.dogs.dequeue();
+      } else {
+        return this.cats.dequeue();
+      }
+    } else if (pref === 'dog') {
+      return this.dogs.dequeue();
+    } else if (pref === 'cat') {
+      return this.cats.dequeue();
+    } else {
+      return null;
+    }
+  }
+}
+
+module.exports = AnimalShelter;
